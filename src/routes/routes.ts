0f62dbf68e16aced8c323cb79712952d833ab2ce
Gabriel Martinez Miranda <gabriel@kubofinanciero.com>
2022-09-14 18:03:26 -0500

    rutas anidadas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 
 import { lazy, LazyExoticComponent } from "react";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 //import { LazyPage1 } from "../01-lazyload/pages";
 //import { LazyPage2 } from '../01-lazyload/pages/LazyPage2';
 //import { LazyPage3 } from '../01-lazyload/pages/LazyPage3';
@@ -15,27 +16,23 @@ interface Route {
   
 }
 
-const Lazy1 =lazy(() => import (/* webpackChunkName: "LazyPage1" */ "../01-lazyload/pages/LazyPage1"));
-const Lazy2 =lazy(() => import (/* webpackChunkName: "LazyPage2" */ "../01-lazyload/pages/LazyPage2"));
-const Lazy3 =lazy(() => import (/* webpackChunkName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3"));
+const LazyLayout =lazy(() => import (/* webpackChunkName: "LazyLayout" */ "../01-lazyload/layout/LazyLayout"));
+//const Lazy1 =lazy(() => import (/* webpackChunkName: "LazyPage1" */ "../01-lazyload/pages/LazyPage1"));
+//const Lazy2 =lazy(() => import (/* webpackChunkName: "LazyPage2" */ "../01-lazyload/pages/LazyPage2"));
+//const Lazy3 =lazy(() => import (/* webpackChunkName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3"));
 
 export const routes:Route[] = [
     {
-        to:"/lazy1",
-        path: "lazy1",
-        Component: Lazy1, 
-        name: "Lazy-1"
+        to:"/lazyload/",
+        path: "/lazyload/*",
+        Component: LazyLayout, 
+        name: "Lazylayout"
     },
     {
-        to:"/lazy2",
-        path: "lazy2",
-        Component: Lazy2,
-        name: "Lazy-2"
+        to:"/no-lazy",
+        path: "no-lazy",
+        Component: NoLazy,
+        name: "nolazy"
     },
-    {
-        to:"/lazy3",
-        path: "lazy3",
-        Component: Lazy3,
-        name: "Lazy-3"
-    },
+   
 ]
